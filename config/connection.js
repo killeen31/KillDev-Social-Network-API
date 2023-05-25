325fdddac96fd9d84ba3732ba730bc15a0d0a306
killeen31 <j31killeen@gmail.com>
2023-05-25 14:31:41 -0400

    User Model

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const { connect, connection } = require('mongoose');
+
+connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
