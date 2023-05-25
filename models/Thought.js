1be6b0bd5318ebeb05cd6ac1238c2e6a60d44744
killeen31 <j31killeen@gmail.com>
2023-05-25 14:45:08 -0400

    Completed Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,37 @@
 const {Schema, model,} = require('mongoose')
+const thoughtSchema = new Schema({
+    thoughtText:{
+        type: String, 
+        required: true,
+        trim: true, 
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt:{
+        type: Date, 
+        default: Date.now,
+        get: (date) => date.toLocaleDateString()
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    reactions: [
+        reactionSchema
+    ]
+
+},{
+    toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+})
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+
+  const Thought = model("thought", thoughtSchema);
+  
+  module.exports = Thought
