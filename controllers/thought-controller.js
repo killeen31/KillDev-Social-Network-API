21f21538a53e6e488e5d2d77e3c2ffb9d4050dd7
killeen31 <j31killeen@gmail.com>
2023-06-01 11:20:37 -0400

    started thought-controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,8 @@
+const { Thought, User, Reaction } = require('../models');
+const {Types} = require('mongoose')
+
+const ThoughtController = {
+    async getAllThoughts(req, res) {
+        try {}
+    }
+}
